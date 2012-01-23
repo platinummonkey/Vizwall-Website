a7e1606f7ac2f830e1a31585c6622f1aabd0dd47
platinummonkey <platinummonkey@archlinux.us>
2012-01-23 09:15:37 -0600

    Cron scripts created

--- system_crons/proctor_reminder.sh
+++ system_crons/proctor_reminder.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # Sends daily reminders to proctors about events they are assigned for the day
 #  - utilizes custom django management command
+#
+# Setup your crontab as follows with `crontab -e`
+#  # sends reminders to proctors about events assigned today every day at 1am
+#  0 1 * * * /vizwall/htdocs/system_crons/proctor_reminder.sh &>/dev/null
+#
 
 pushd /vizwall/htdocs/vizwall &> /dev/null
 python manage.py cron_proctor_reminder &> /dev/null
